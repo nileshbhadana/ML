8c9e69987234672915cb912a67f102cdd4fd46f4
nileshbhadana <nileshbhadana3@gmail.com>
2018-12-27 07:04:02 +0530

    facebook login through FaceRecognition

--- face_recognition_recognizer.py
+++ face_recognition_recognizer.py
@@ -5,15 +5,24 @@ Created on Thu Dec 27 05:19:44 2018
 
 @author: nilesh
 """
-
+#importing libraries
 import cv2,time
 import numpy as np
 
+#setting font for puttext
 font=cv2.FONT_HERSHEY_SIMPLEX
+
+#loading and reading data from the saved training files
 face_data=np.load('training_faces.npy')
 labels=np.load('training_labels.npy')
+
+#creating recognizer
 recognizer=cv2.face.LBPHFaceRecognizer_create()
+
+#training the recognizer
 recognizer.train(face_data,np.array(labels))
+
+#flag for the detected face
 detected=0
 #cascading
 cascade=cv2.CascadeClassifier('face.xml')
@@ -29,10 +38,18 @@ while cam.isOpened():
     for (x,y,w,h) in faces:
         #drawing rectangle on the faces
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
+        
+        #predicting the label and confidence
         label,confidence=recognizer.predict(gray_frame[y:y+h,x:x+w])
+        
+        #checking for confidence (the lower the confidence the more accurate the prediction is)
         if confidence<30:
             msg="Face Detected"
+            
+            #printing the message 
             cv2.putText(frame,msg,(20,50),font,1,(255,255,255),3,cv2.LINE_AA)
+            
+            #changing flag to 1
             detected=1
     cv2.imshow('live',frame)
 
@@ -45,15 +62,32 @@ while cam.isOpened():
 cv2.destroyAllWindows()
 cam.release()
 
+
+#checking whether face is detected or not
 if detected==1:
+    
+    #importing selenium library 
     from selenium import webdriver
+    
+    #creating driver instance
     driver = webdriver.Chrome(executable_path='/home/nilesh/Downloads/chromedriver')
+    
+    #username for facebook
     user="nileshbhadana"
+    #password for facebook
     passwd="esehithodebtadunga"
+    
+    #opening window and fetching the facebook
     driver.get('https://www.facebook.com')
+    
+    #finding elements by id on the webpage
     username_box=driver.find_element_by_id('email')
     password_box=driver.find_element_by_id('pass')
     login_button=driver.find_element_by_id('loginbutton')
+    
+    #filling the username and password
     username_box.send_keys(user)
     password_box.send_keys(passwd)
+    
+    #clicking the login button
     login_button.click()
