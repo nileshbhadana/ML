0abe9a0ec8a104ffa270568bac83295e6945f991
nileshbhadana <nileshbhadana3@gmail.com>
2019-01-20 01:12:13 +0530

    Stock prediction using SVR

--- stock_prediction.py
+++ stock_prediction.py
@@ -8,64 +8,48 @@ Created on Sat Jan 19 02:22:04 2019
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-#import csv  
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
+from sklearn import svm
 from sklearn import linear_model
+import matplotlib.pyplot as plt
 
+df=pd.read_csv("/home/nilesh/Desktop/ML/stock_data.csv")
 
-def prepare_data(df,forecast_col,forecast_out,test_size):
-    label = df[forecast_col].shift(-forecast_out)#creating new column called label with the last 5 rows are nan
-    X = np.array(df[[forecast_col]]); #creating the feature array
-    print(X)
-    X = preprocessing.scale(X) #processing the feature array
-    print(X)
-    X_lately = X[-forecast_out:] #creating the column i want to use later in the predicting method
-    X = X[:-forecast_out] # X that will contain the training and testing
-    label.dropna(inplace=True) #dropping na values
-    y = np.array(label)  # assigning Y
-    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3) #cross validation 
+price_data_obj=df['Close']
+price_data=price_data_obj.values
+price_df=price_data.reshape(-1,1)
 
-    response = [X_train,X_test , Y_train, Y_test , X_lately]
-    return response;
+day=[]
+for i in range(len(price_df)):
+    day.append(i)
 
+day_count=np.array(day).reshape(-1,1)
+print(type(day),len(day))
 
 
 
+price_data_train=price_df[:-5]
+price_data_test=price_df[-5:]
 
-df = pd.read_csv("/home/nilesh/Desktop/ML/stock_data_5Y.csv") #loading the csv file
-'''
-dates=[]
-prices=[]
-with open('/home/nilesh/Desktop/stock_data.csv', 'r') as csvfile:
-    csvFileReader = csv.reader(csvfile)
-    next(csvFileReader) # skipping column names
-    for row in csvFileReader:
-        dates.append(row[0])
-        prices.append(float(row[3]))
-print(dates)
-print(prices)'''
+print(len(price_data_train),len(price_data_test))
 
-forecast_col = 'Close' #choosing which column to forecast
-forecast_out = 5 #how far to forecast 
-test_size = 0.2 #the size of my test set
+day_target_train=day_count[:-5]
+day_target_test=day_count[-5:]
 
-X_train, X_test, Y_train, Y_test , X_lately =prepare_data(df,forecast_col,forecast_out,test_size) #calling the method were the cross validation and data preperation is in
+regr=linear_model.LinearRegression()
+regr_svm=svm.SVR()
 
-learner = linear_model.LinearRegression(); #initializing linear regression model
+regr.fit(day_target_train,price_data_train)
+regr_svm.fit(day_target_train,price_data_train)
 
-learner.fit(X_train,Y_train) #training the linear regression model
-score=learner.score(X_test,Y_test)#testing the linear regression model
-
-forecast= learner.predict(X_lately) #set that will contain the forecasted data
-
-#print(len(X_train),len(df['Date']),len(Y_train),len(X_test),len(Y_test))
-#print(X_train)
-#print(Y_train)
-print(forecast)
-print(score)
+prediction=regr.predict(day_target_test)
+prediction_svm=regr_svm.predict(day_target_test)
 
 
+print(prediction)
+print(prediction_svm)
+print(price_data_test)
 
-#creating new column called label with the last 5 rows are nan
-    
+plt.plot(day_target_train,price_data_train,'r')
+plt.plot(day_target_test,price_data_test,'g')
+plt.plot(day_target_test,prediction_svm,'b')
+plt.show()
